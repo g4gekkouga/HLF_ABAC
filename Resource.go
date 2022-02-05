ec0f8c550e9b27ffd5393fd705228dcd8e6e159f
amshumaan <amshumaan6pericherla@gmail.com>
2022-02-06 00:25:28 +0530

    Added Resource Methods

--- Resource.go
+++ Resource.go
@@ -14,15 +14,29 @@ type Resource struct {
 	Data       string `json:"data"`
 }
 
-func AddResource(ctx contractapi.TransactionContextInterface, resourceID string, data string) (string, error) {
+func addResource(ctx contractapi.TransactionContextInterface, resourceID string, data string, collection string) (string, error) {
 
 	if len(resourceID) == 0 {
 		return "", fmt.Errorf("Please enter valid Resource ID")
 	}
 
+	ownerID, err := GetSubmittingClientIdentity(ctx)
+
+	if err != nil {
+		return "", err
+	}
+
+	ownerKey, err := GetSubmittingClientPubKey(ctx)
+
+	if err != nil {
+		return "", err
+	}
+
 	resource := Resource{
-		RID:  resourceID,
-		Data: data,
+		ResourceID: resourceID,
+		OwnerID:    ownerID,
+		OwnerKey:   ownerKey,
+		Data:       data,
 	}
 
 	resourceJSON, err := json.Marshal(resource)
@@ -32,5 +46,63 @@ func AddResource(ctx contractapi.TransactionContextInterface, resourceID string,
 
 	ctx.GetStub().SetEvent("AddResource", resourceJSON)
 
-	return ctx.GetStub().GetTxID(), ctx.GetStub().PutState(resourceID, resourceJSON)
+	return ctx.GetStub().GetTxID(), ctx.GetStub().PutPrivateData(collection, resourceID, resourceJSON)
+}
+
+func updateResource(ctx contractapi.TransactionContextInterface, resourceID string, data string, collection string) (string, error) {
+
+	if len(resourceID) == 0 {
+		return "", fmt.Errorf("Please enter valid Resource ID")
+	}
+
+	resourceJSON, err := ctx.GetStub().GetPrivateData(collection, resourceID)
+
+	if resourceJSON == nil {
+		return "", fmt.Errorf("No Resource with given ID")
+	}
+
+	ownerID, err := GetSubmittingClientIdentity(ctx)
+
+	if err != nil {
+		return "", err
+	}
+
+	ownerKey, err := GetSubmittingClientPubKey(ctx)
+
+	if err != nil {
+		return "", err
+	}
+
+	resource := Resource{
+		ResourceID: resourceID,
+		OwnerID:    ownerID,
+		OwnerKey:   ownerKey,
+		Data:       data,
+	}
+
+	resourceJSON, err := json.Marshal(resource)
+	if err != nil {
+		return "", err
+	}
+
+	ctx.GetStub().SetEvent("UpdateResource", resourceJSON)
+
+	return ctx.GetStub().GetTxID(), ctx.GetStub().PutPrivateData(collection, resourceID, resourceJSON)
+}
+
+func getResource(ctx contractapi.TransactionContextInterface, resourceID string, collection string) *Resource {
+	if len(resourceID) == 0 {
+		return nil
+	}
+
+	resourceJSON, err := ctx.GetStub().GetPrivateData(collection, resourceID)
+
+	if err != nil || resourceJSON == nil {
+		return nil
+	}
+
+	resource := new(Resource)
+	_ = json.Unmarshal(resourceJSON, resource)
+
+	return resource
 }
